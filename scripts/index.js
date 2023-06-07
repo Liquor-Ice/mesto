5d3703206d4eb0ba1de1d067b9815e9ef59cecb0
Liquor-Ice <doppelgangerer@yandex.ru>
2023-06-07 22:56:57 +0300

    Исправляет ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ const profile = document.querySelector('.profile');
 const popup = document.querySelector('.popup');
 const editButton = profile.querySelector('.profile__edit');
 const closeButton = popup.querySelector('.popup__close');
-const submitButton = popup.querySelector('.popup__button');
 let profileName = profile.querySelector('.profile__name');
 let job = profile.querySelector('.profile__job');
 let nameInput = popup.querySelector('.popup__input_type_name');
@@ -17,8 +16,10 @@ editButton.addEventListener('click', function () {
   jobInput.value = job.textContent;
   togglePopup();
 });
+
 closeButton.addEventListener('click', togglePopup);
-submitButton.addEventListener('click', function () {
+
+popup.addEventListener('submit', function () {
   profileName.textContent = nameInput.value;
   job.textContent = jobInput.value;
   togglePopup();
